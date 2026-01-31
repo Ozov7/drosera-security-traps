dce66fc4d59d6bd1c007d5a9f955228b0ca3690a
Ozov7 <114500806+Ozov7@users.noreply.github.com>
2026-01-31 07:41:41 +0100

    Add tests for MEVSandwichDetector contract

--- test/MEVSandwichDetector.t.sol
+++ test/MEVSandwichDetector.t.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "forge-std/Test.sol";
+import "../src/traps/MEVSandwichDetector.sol";
+
+contract MEVSandwichDetectorTest is Test {
+    MEVSandwichDetector detector;
+    
+    function setUp() public {
+        detector = new MEVSandwichDetector();
+    }
+    
+    function testConstants() public {
+        assertEq(detector.UNISWAP_POOL(), address(0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640));
+        assertEq(detector.MIN_PRICE_IMPACT_BPS(), 50);
+        assertEq(detector.MIN_PROFIT_ETH(), 0.1 ether);
+    }
+    
+    function testPlannerSafety() public {
+        // Test empty data
+        bytes[] memory emptyData = new bytes[](0);
+        (bool shouldTrigger, bytes memory response) = detector.shouldRespond(emptyData);
+        
+        assertFalse(shouldTrigger, "Should not trigger on empty data");
+        assertEq(response.length, 0, "Response should be empty");
+    }
+    
+    function testConstructor() public view {
+        // Just ensure it deploys without revert
+        address(detector);
+    }
+}
