794f482b776de00c15a4d0f02f921d30722d2887
Ozov7 <114500806+Ozov7@users.noreply.github.com>
2026-01-31 07:54:29 +0100

    Add minimal Trap interface and abstract contract

--- src/interfaces/ITrap.sol
+++ src/interfaces/ITrap.sol
@@ -1,1 +1,26 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Minimal Trap interface for compilation
+// Will be replaced with real drosera-contracts later
+interface ITrap {
+    struct Log {
+        bytes32[] topics;
+        bytes data;
+    }
+    
+    function _addEventFilter(address, bytes32) external;
+    function getFilteredLogs() external view returns (Log[] memory);
+}
+
+abstract contract Trap is ITrap {
+    function collect() external virtual view returns (bytes memory);
+    function shouldRespond(bytes[] calldata data) external virtual pure returns (bool, bytes memory);
+    
+    // Minimal implementations for compilation
+    function _addEventFilter(address, bytes32) internal virtual {}
+    function getFilteredLogs() internal view virtual returns (Log[] memory) {
+        return new Log[](0);
+    }
+}
