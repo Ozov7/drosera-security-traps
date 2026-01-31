5e3be865897413cb099db3c26e2c65f893fad4e0
Ozov7 <114500806+Ozov7@users.noreply.github.com>
2026-01-31 11:22:12 +0100

    Update ITrap.sol

--- src/interfaces/ITrap.sol
+++ src/interfaces/ITrap.sol
@@ -1,26 +1,34 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 // Minimal Trap interface for compilation
-// Will be replaced with real drosera-contracts later
-interface ITrap {
+// Will be replaced with real drosera-contracts in production
+
+abstract contract ITrap {
     struct Log {
         bytes32[] topics;
         bytes data;
     }
     
-    function _addEventFilter(address, bytes32) external;
-    function getFilteredLogs() external view returns (Log[] memory);
-}
-
-abstract contract Trap is ITrap {
+    function _addEventFilter(address, bytes32) external virtual;
+    function getFilteredLogs() external view virtual returns (Log[] memory);
     function collect() external virtual view returns (bytes memory);
     function shouldRespond(bytes[] calldata data) external virtual pure returns (bool, bytes memory);
-    
+}
+
+contract Trap is ITrap {
     // Minimal implementations for compilation
-    function _addEventFilter(address, bytes32) internal virtual {}
-    function getFilteredLogs() internal view virtual returns (Log[] memory) {
+    function _addEventFilter(address, bytes32) internal override {}
+    
+    function getFilteredLogs() internal view override returns (Log[] memory) {
         return new Log[](0);
+    }
+    
+    function collect() external view override returns (bytes memory) {
+        return "";
+    }
+    
+    function shouldRespond(bytes[] calldata) external pure override returns (bool, bytes memory) {
+        return (false, "");
     }
 }
