4dc80523156820f08286a400db33e815c9e8ff26
Ozov7 <114500806+Ozov7@users.noreply.github.com>
2026-01-31 14:47:11 +0100

    Update MEVSandwichDetector.t.sol

--- test/MEVSandwichDetector.t.sol
+++ test/MEVSandwichDetector.t.sol
@@ -20,7 +20,7 @@ contract MEVSandwichDetectorTest is Test {
     function testPlannerSafety() public {
         // Test empty data
         bytes[] memory emptyData = new bytes[](0);
-        (bool shouldTrigger, bytes memory response) = detector.shouldRespond(emptyData);
+        (bool shouldTrigger, bytes memory response) = detector.evaluateResponse(emptyData);
         
         assertFalse(shouldTrigger, "Should not trigger on empty data");
         assertEq(response.length, 0, "Response should be empty");
@@ -29,5 +29,32 @@ contract MEVSandwichDetectorTest is Test {
     function testConstructor() public view {
         // Just ensure it deploys without revert
         address(detector);
+    }
+    
+    function testInsufficientSwaps() public {
+        // Create dummy swap data with only 2 swaps
+        MEVSandwichDetector.SwapData[] memory swaps = new MEVSandwichDetector.SwapData[](2);
+        swaps[0] = MEVSandwichDetector.SwapData({
+            sender: address(0x1),
+            amount0: 1000,
+            amount1: -1000,
+            blockNumber: 100,
+            timestamp: block.timestamp,
+            isExactInput: true
+        });
+        swaps[1] = MEVSandwichDetector.SwapData({
+            sender: address(0x2),
+            amount0: -1000,
+            amount1: 1000,
+            blockNumber: 100,
+            timestamp: block.timestamp,
+            isExactInput: false
+        });
+        
+        bytes[] memory data = new bytes[](1);
+        data[0] = abi.encode(swaps);
+        
+        (bool shouldTrigger, ) = detector.evaluateResponse(data);
+        assertFalse(shouldTrigger, "Should not trigger with only 2 swaps");
     }
 }
